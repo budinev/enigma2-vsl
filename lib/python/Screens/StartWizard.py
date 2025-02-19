96a667ea6b8dabd92de6db0abb575f55eebfb45b
daimon <jaikan@hotmail.it>
2025-02-19 11:13:00 +0100

    add skin loi

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -23,7 +23,7 @@ import os
 
 config.misc.firstrun = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
-config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard and config.skin.primary_skin.value == "pDreamy-FHD/skin.xml")
+config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard and config.skin.primary_skin.value == "pd1loi-HD-night/skin.xml")
 
 class StartWizard(WizardLanguage, Rc):
 	def __init__(self, session, silent=True, showSteps=False, neededTag=None):

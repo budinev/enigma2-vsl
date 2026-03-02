4304580af98ad9cd7c947b565a9913bf911fb67e
WXbet <57314510+WXbet@users.noreply.github.com>
2026-03-02 15:57:53 +0100

    [SoftCSA] Add decoder start delay function
    Add a configurable pre-buffer time (0-2000ms) that delays decoder start,
    allowing descrambled data to accumulate in the DVR kernel buffer. When
    the decoder finally starts, both video and audio data are immediately
    available, preventing AC3 audio dropouts caused by the PVR loopback
    timing gap.
    Cherry-picked from openatv/enigma2 e0c0b61989

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -838,6 +838,10 @@ def InitUsageConfig():
 		default=800,
 		choices=[(0, _("Disabled"))] + [(x, _("%d ms") % x) for x in range(100, 2001, 100)]
 	)
+	config.softcsa.bufferTime = ConfigSelection(
+		default=0,
+		choices=[(0, _("Disabled"))] + [(x, _("%d ms") % x) for x in range(100, 2001, 100)]
+	)
 	config.softcsa.useStreamRelayWhitelist = ConfigYesNo(default=True)
 
 	config.ntp = ConfigSubsection()
